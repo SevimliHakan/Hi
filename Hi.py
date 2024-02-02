530a709e0b22b0c65026887539a5e915c0ff97de
Hakan <Hakan@PC01>
2024-02-02 22:33:30 +0300

    Add project files.

---
+++ Hi.py
@@ -0,0 +1,49 @@
+yazar="Hakan"
+ugurlusayi1="8"
+ugurlusayi2="7"
+ugurlusayi1sayi=int(ugurlusayi1)
+ugurlusayi2sayi=int(ugurlusayi2)
+ugurlusayitoplami=ugurlusayi1sayi+ugurlusayi2sayi
+#int=sayý str=yazý print(type(dýtdýtdýt))=dýtdýtdýt int or str
+
+ugurlusayitoplamiyaziIle=str(ugurlusayitoplami)
+
+#birþeyi = ile onu farklý birþey olarak iþaretlersen +farklý birþey dersen oto birþey yazar
+print("hey")
+print("Hello "+yazar+" !")
+print("")
+# print comudu ekrana yazar
+print(yazar+"\'in ugurlu sayisi "+ ugurlusayi1 +"\n iki sayinin toplami "+ugurlusayitoplamiyaziIle )
+
+
+
+print("Hakan\'in defteri. \nBu da ikinci satir")#\ alt gr  ./, gibi þeyleri yazmamýzý saðlar
+print("\\")
+ 
+
+#=yazma=uygulama
+
+#\n=new line=alt satuir
+
+
+isim=input("lutfen adinizi giriniz ")
+a=input("Lutfen bir rakam giriniz")
+b=input("Lutfen bir rakam daha giriniz")
+a2=int(a)
+b2=int(b)
+c2=a2+b2
+c=str(c2)
+d=a2*b2
+d2=str(d)
+e=a2//b2
+e3=str(e)
+f=a2%b2
+f2=str(f)
+print("")
+print("girilen isim "+isim)
+print("girilen rakamlarin toplami "+c)
+print("girilen rakamlarin carpimi "+d2)
+print("girilen rakamlarin bolumu "+e3)
+print("girilen rakamlarin kalani "+f2)
+#input=al input ile yapýlacak þey inpoutdan once olamaz   *=çarpma
+#float=küsürlü sayý /=bölme %=kalan //= floatsuz bölüm
